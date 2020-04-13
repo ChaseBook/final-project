8b1ae8adf629e3d7301cc655e7158a9bd0df8285
Chase Bookin <cbookin@college.harvard.edu>
2020-04-12 18:10:40 -0700

    added career peak distribution to shiny

--- final_project_shiny/app.R
+++ final_project_shiny/app.R
@@ -6,11 +6,15 @@ library(tidyverse)
 library(gt)
 
 
+master <- read_csv("csv/project_master.csv")
+redraft_table <- read_csv("csv/redraft_table.csv")
+
 ui <- fluidPage(theme = shinytheme("flatly"),
                 
             
                 navbarPage("Exploring the MLB Draft",
-                           
+ 
+ # Introduction tab -------------------------------                                                    
                            tabPanel("Introduction", align = "center",
                                     
                               titlePanel("High School Players and The Major League Baseball Draft"),
@@ -24,12 +28,94 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                               
                               h2("About the Project", align = "center"),
                               
-                              htmlOutput("intro")),
+                              htmlOutput("intro")
+                           ),
                            
-                           tabPanel("Turning Down the Draft", align = "center"),
+ # Turning Down the Draft Tab -------------------------
+                           tabPanel("Turning Down the Draft", align = "center",
+                                    
+                                    
+                                    tabsetPanel(
+                                      
+                                      tabPanel("Chances of Getting Redrafted",
+                                                
+                                                titlePanel("What are the Chances of Getting Redrafted?"),
+                                                
+                                                br(),
+                                                br(),
+                                    
+                                                sidebarLayout(
+                                                  sidebarPanel(
+                                                    
+                                                    checkboxGroupInput("redraft_year",
+                                                                       "Start of Five Year Period:",
+                                                                       unique(redraft_table$start_year), 
+                                                                       selected = c(1990, 1995,
+                                                                                    2000, 2005, 2010)
+                                                    )
+                                                  ),
+                                                  mainPanel(
+                                                    plotOutput("redraftProb")
+                                                  )
+                                                )
+                                       ),
+                                    
+                                      tabPanel("Change in Draft Stock",
+                                               
+                                               titlePanel("How Does Draft Stock Change when a Player is Redrafted?"),
+                                               
+                                               br(),
+                                               br(),
+                                               
+                                               sidebarLayout(
+                                                 sidebarPanel(
+                                                   
+                                                   checkboxGroupInput("stock_year",
+                                                                      "Start of Five Year Period:",
+                                                                      unique(redraft_table$start_year), 
+                                                                      selected = c(1990, 1995,
+                                                                                   2000, 2005, 2010)
+                                                   )
+                                                 ),
+                                                 mainPanel(
+                                                   plotOutput("redraftStock")
+                                                 )
+                                               )
+                                        
+                                      )
+                                    )
+                           ), 
+ 
+ # Navigating the Minors Tab -----------------------
                            
-                           tabPanel("Navigating the Minors", align = "center"),
+                           tabPanel("Navigating the Minors", align = "center",
+                                    
+                                    titlePanel("Path to the Show"),
+                                    
+                                    br(),
+                                    br(),
+                                    
+                                    sidebarLayout(
+                                      sidebarPanel(
+                                        
+                                        numericInput("peakroundMin", "Minimum Round:", value = 1, min = 1, max = 40),
+                                        numericInput("peakroundMax", "Maximum Round:", value = 20, min = 1, max = 40),
+                                        numericInput("peakyearMin", "Minimum Year:", value = 1990, min = 1965, max = 2010),
+                                        numericInput("peakyearMax", "Maximum Year:", value = 2010, min = 1965, max = 2010)
+                                      ),
+                                      
+                                      mainPanel(
+                                        plotOutput("careerPeak")
+                                      )
+                                    )
                            
+                           ),
+                                    
+                                    
+                                    
+
+ # Major League Success Tab -------------------------
+ 
                            tabPanel("Major League Success", align = "center")
                            
                                        
@@ -45,6 +131,7 @@ server <- function(input, output) {
   library(directlabels)
   
   
+# Introduction
   
   output$intro<-renderUI({
     
@@ -81,6 +168,104 @@ server <- function(input, output) {
     HTML(paste(ex_1, br(),ex_2, br(), ex_3))
     
   })
+    
+# Getting Re-drafted 
+  
+# Re-draft probability graph
+  
+  redraft_subset <- reactive({redraft_table%>% filter(start_year %in%input$redraft_year)})
+  
+  output$redraftProb <- renderPlot({
+      
+    redraft_subset() %>%
+      ggplot(aes(round, percent_redrafted)) +
+      geom_point(na.rm = TRUE) +
+      geom_line(aes(group = start_year, color = start_year), na.rm = TRUE) +
+      labs(
+        x = "Round Range Drafted out of High School",
+        y = "Percent of Picks Re-drafted",
+        color = "Time Frame",
+        title = "High School Re-draft Probability",
+        subtitle = "Organized by initial round range and time period"
+      ) +
+      theme_light() +
+      theme(text = element_text(size=20)) +
+      scale_color_viridis_c() +
+      scale_x_continuous(breaks = c(5, 10, 20, 30, 40, 50))
+  })
+    
+# Re-draft stock graph
+  
+  stock_subset <- reactive({redraft_table%>% filter(start_year %in%input$stock_year)})  
+  
+  output$redraftStock <- renderPlot({
+      
+    redraft_table %>%
+      ggplot(aes(round, mean_round_diff)) +
+      geom_point(na.rm = TRUE) +
+      geom_line(aes(group = start_year, color = start_year), na.rm = TRUE) +
+      labs(
+        x = "Round Range Drafted out of High School",
+        y = "Average Improvement Re-draft Round",
+        color = "Time Frame",
+        title = "Average Change in Round of Re-drafted Unsigned High School Picks",
+        subtitle = "Organized by initial round range and time period"
+      ) +
+      theme_light() +
+      theme(text = element_text(size=20)) +
+      scale_color_viridis_c() +
+      scale_x_continuous(breaks = c(5, 10, 20, 30, 40, 50)) +
+      scale_y_continuous(breaks = c(-10, -5, 0, 10, 20)) +
+      geom_hline(color = "red", linetype = "dashed", yintercept = 0)
+    })
+  
+# Career peak distribution
+  
+  
+  peak_subset <- reactive({master %>%
+      filter(source == "H" & draft_numb == 1) %>%
+      filter(draft_round >= input$peakroundMin & draft_round <= input$peakroundMax 
+             & year >= input$peakyearMin & year <= input$peakyearMax) %>%
+      mutate(high_level = ifelse(high_level %in% c("-", "JrCollege", "NAIA", "NCAA"),
+                                 "<=College",
+                                 high_level)) %>%
+      group_by(signed, high_level) %>%
+      summarize(n = n()) %>%
+      group_by(signed) %>%
+      mutate(y = n/sum(n)) %>%
+      mutate(high_level = factor(high_level,levels = c("<=College",
+                                                       "Indy",
+                                                       "Rk",
+                                                       "A-",
+                                                       "A",
+                                                       "A+",
+                                                       "AA",
+                                                       "AAA",
+                                                       "MLB")))
+                                
+  })
+  
+  output$careerPeak <- renderPlot({
+    
+    peak_subset() %>%
+      ggplot() +
+      geom_col(aes(x = high_level, y = y, fill = signed), position = "dodge") +
+      scale_y_continuous(labels = scales::percent_format()) +
+      theme_light() +
+      theme(text = element_text(size=20)) +
+      labs(
+        x = "Peak Baseball Level Reached",
+        y = "Percentage",
+        fill = "Signed?",
+        title = "Distribution of Peak Baseball Career Level of High School Picks",
+        subtitle = "Grouped by whether or not the player signed out of high school"
+      )
+    
+  })
+  
+
+        
+    
 }
 
 
